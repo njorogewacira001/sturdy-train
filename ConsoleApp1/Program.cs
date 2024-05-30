a27a722942ad0c953201f69d5778b96b7a4a8b1b
Gavin <gavinwacira@gmail.com>
2024-05-30 17:22:46 +0300

    first commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// reference parameter
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 10;
+        ModifyValue(ref x);
+        Console.WriteLine("Value after method call: " + x);  // Output: 20
+    }
+
+    static void ModifyValue(ref int a)
+    {
+        a = 20;
+    }
+}
